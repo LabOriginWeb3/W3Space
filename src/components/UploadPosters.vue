b672f143597239659fe753dade86d46389bbb92e
cuegod <hengxingtianxia@gmail.com>
2023-04-24 14:12:25 +0800

    update uploaPoster ui

--- src/components/UploadPosters.vue
+++ src/components/UploadPosters.vue
@@ -1,46 +1,37 @@
 <template>
   <div class="officeBg uploadLogo" @click="closeUploadPosters">
     <div class="officeCon" @click="cancelBubble($event)">
-      <p class="close_frame" @click="closeUploadPosters">X</p>
+      <p class="close_frame" @click="closeUploadPosters">
+        <img src="../assets/close.svg" />
+      </p>
       <div class="bgImg">
         <img class="left" src="../assets/bg-left.png" />
       </div>
       <div class="info">
         <img :src="logoSrc" style="width: 200px; height: 200px" />
         <img
-          v-show="logoSrc === null || logoSrc === ''"
-          class="addLogo"
-          style="position: absolute"
-          src="../assets/addLogo.svg"
+            v-show="logoSrc === null || logoSrc === ''"
+            class="addLogo"
+            style="position: absolute"
+            src="../assets/addLogo.svg"
         />
         <input
-          class="filePrew"
-          type="file"
-          accept=".jpg,.jpeg,.png"
-          @change="addLogo($event, '')"
-          ref="logoImg"
+            class="filePrew"
+            type="file"
+            accept=".jpg,.jpeg,.png"
+            @change="addLogo($event, '')"
+            ref="logoImg"
         />
         <input
-          id="oldimg"
-          name="oldimg"
-          v-model="oldimg"
-          value=""
-          type="hidden"
+            id="oldimg"
+            name="oldimg"
+            v-model="oldimg"
+            value=""
+            type="hidden"
         />
       </div>
 
       <div class="enterButton" @click="uploadLogo">
-        <div class="containerline">
-          <svg
-            height="100%"
-            width="100%"
-            xmlns="http://www.w3.org/2000/svg"
-            style="border-radius: 10px"
-          >
-            <rect class="outline" height="100%" width="100%" rx="10" />
-          </svg>
-        </div>
-        <div class="container"></div>
         <div class="text">UPLOAD</div>
       </div>
       <div class="bgImg bgImg_right">
@@ -71,10 +62,19 @@ export default {
   created() {
     let address = window.location.href.split("=");
     this.roomName = address[1];
-    if (window.location.href.indexOf("w3.work") > -1) {
-      this.locationUrl = "https://alpha.w3.work";
+
+    if (window.location.href.indexOf("w3space") > -1) {
+      if (window.location.href.indexOf("w3space.co") > -1) {
+        this.locationUrl = "https://alpha.w3space.co";
+      } else {
+        this.locationUrl = "https://alpha.w3space.org";
+      }
     } else {
-      this.locationUrl = "https://alpha.w3work.org";
+      if (window.location.href.indexOf("w3.work") > -1) {
+        this.locationUrl = "https://alpha.w3.work";
+      } else {
+        this.locationUrl = "https://alpha.w3work.org";
+      }
     }
   },
   methods: {
@@ -109,22 +109,22 @@ export default {
         formData.append("oldimg", JSON.stringify(this.oldimg));
         let url = this.locationUrl + "/meeting/index.php";
         axios
-          .post(url, formData, {
-            headers: { "Content-Type": "multipart/form-data" },
-          })
-          .then((res) => {
-            if (res.data !== "error") {
-              let params = {
-                name: res.data,
-                meeting: this.roomName,
-                id: this.postersId,
-              };
-              this.$emit("updateMeetingBill", params);
-              this.logoFile = "";
-              this.logoSrc = "";
-              this.$emit("closeUploadPosters", false);
-            }
-          });
+            .post(url, formData, {
+              headers: { "Content-Type": "multipart/form-data" },
+            })
+            .then((res) => {
+              if (res.data !== "error") {
+                let params = {
+                  name: res.data,
+                  meeting: this.roomName,
+                  id: this.postersId,
+                };
+                this.$emit("updateMeetingBill", params);
+                this.logoFile = "";
+                this.logoSrc = "";
+                this.$emit("closeUploadPosters", false);
+              }
+            });
       }
     },
     closeUploadPosters() {
